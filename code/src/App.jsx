2d7bf7ffba420c566e3d11ad79b96c3a2f096ec6
amir chapi <amirmalka26@gmail.comgit config --global user.name amir>
2025-03-13 19:25:26 +0200

    final

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Greeting from './components/Greeting'
 import Counter from './components/Counter'
 
-export default function App({name}) {
+export default function App({name, newName}) {
   return (
     <div>
       <Greeting name = "david"/>
       <Counter />
+      <newName  newName = "amir"/>
     </div>
   )
 }
---
+++ code/src/NewGreeting.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NewGreeting({newName}) {
+  return (
+    <div>
+      hello {newName}
+    </div>
+  )
+}
