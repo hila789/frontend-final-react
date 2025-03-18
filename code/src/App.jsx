ba0985da36072f86648e34abca91f7d8514d27d8
amir chapi <amirmalka26@gmail.comgit config --global user.name amir>
2025-03-18 22:16:35 +0200

    Routs

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -4,15 +4,19 @@ import Counter from './components/Counter'
 import Header from './components/Header'
 import MSGManage from './components/MSGManage'
 import { Routes, Route } from 'react-router-dom'
+import TaskForm from './components/TaskForm'
 
-export default function App({ name, newName }) {
+
+export default function App() {
   return (
     <div>
 
       <Header />
+      <Counter/>
+      <Greeting/>
       <Routes>
-        
-        <Route path="/MSGManage" element={<MSGManage />} />
+
+        <Route path="/TaskForm" element={<TaskForm/>} />
       </Routes>
     </div>
   )
